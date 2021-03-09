829cfca38d9e926c56f2a8115fdd316347efdc9e
pc <pc@DESKTOP-FRU0P3Q.mshome.net>
2021-03-09 23:59:21 +0530

    Gradle project

---
+++ src/main/java/com/example/gradle/demoGradleTest/DemoGradleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle.demoGradleTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradle/demoGradleTest/DemoGradleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradle.demoGradleTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
